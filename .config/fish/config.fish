222bcffa1990640dac13a60844977d72425d89d1
Rex <hackertstark@gmail.com>
2025-07-14 18:19:38 +0530

    Initial commit with all config files and folders

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+   oh-my-posh init fish --config /home/rex/.config/fish/1_shell.omp.json | source
+fastfetch --config /home/rex/.config/fish/config.jsonc
+set fish_greeting " "
+
+end
